283f819ddea39dcd3d6e8de443a9f9dbf26ca0e4
Hector Sanchez <hsanchez@opentext.com>
2022-09-16 12:19:44 -0600

    Add express server file, Procfile, and readme

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+
+const requireHTTPS = (request, response, next) => {
+	if (request.header('x-forwarded-proto') != 'https') {
+		return response.redirect(
+			`https://${request.header('host')}${request.url}`
+		)
+	}
+	return next()
+}
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(requireHTTPS)
+}
+
+app.set('port', process.env.PORT || 3000)
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.locals.title = 'Sine Wave'
+
+app.listen(app.get('port'), () => {
+	console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+})
+
+module.exports = app
